5c814da0ec8108550ee8bd65070247baf2999393
Venom (Mel Macaluso) <mel@capeeshe.com>
2020-10-28 16:44:52 +0000

    adding routing and splitting logic

--- lawyer-vl-ui/src/apps/DesktopApp/components/Main.tsx
+++ lawyer-vl-ui/src/apps/DesktopApp/components/Main.tsx
@@ -1,15 +1,15 @@
 //@ts-nocheck
-import React, {useEffect, useState} from 'react';
-import {Grid, Paper, TextField, Box, createStyles, Theme} from '@material-ui/core';
-import {makeStyles} from '@material-ui/core/styles';
-import {LetterParagraph} from './LetterParagraph';
-import { updateAll } from '../data/paragraphsDataSlice'
+import React, { useEffect, useState } from 'react';
+import { Grid, Paper, TextField, Box, createStyles, Theme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+import { LetterParagraph } from './LetterParagraph';
+import { updateAll } from '../../../data/paragraphsDataSlice'
 import { useSelector, useDispatch } from 'react-redux';
-import {getData} from '../api/vlmasersheet';
-import AppState from '../data/AppState';
-import {Paragraph} from '../data/types';
-import {Filter} from './Filter';
-import {filterByExactTopicMatch, filterByGeneralMatch, filterByOrMatch} from '../filters';
+import { getData } from '../../../api/vlmasersheet';
+import AppState from '../../../data/AppState';
+import { Paragraph } from '../../../data/types';
+import { Filter } from './Filter';
+import { filterByExactTopicMatch, filterByGeneralMatch, filterByOrMatch } from '../../../filters';
 
 interface Props {
 
@@ -20,7 +20,7 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
         flexGrow: 1,
         background: theme.palette.background.default,
         padding: '1rem',
-        paddingTop:'2rem'
+        paddingTop: '2rem'
     },
     paper: {
         height: '100%',
@@ -30,17 +30,17 @@ const useStyles = makeStyles((theme: Theme) => createStyles({
     },
 }));
 
-export const Main: React.FC<Props>= (props: Props) => {
+export const Main: React.FC<Props> = (props: Props) => {
 
     const classes = useStyles();
     const dispatch = useDispatch();
-    const data  = useSelector<AppState>((state) => state.paragraphs.all);
+    const data = useSelector<AppState>((state) => state.paragraphs.all);
     const [filteredData, setFilteredData] = useState<Paragraph[]>(data ?? []);
-    console.log('The  filtered data  is:  ' ,  filteredData);
+    console.log('The  filtered data  is:  ', filteredData);
 
     // filter for exact match
     const [filter, setFilter] = useState<string>(null);
-    const [orFitler, setOrFitler] = useState<string[]>([] );
+    const [orFitler, setOrFitler] = useState<string[]>([]);
 
     const matches = filteredData?.length
 
@@ -53,13 +53,13 @@ export const Main: React.FC<Props>= (props: Props) => {
     }
 
     useEffect(() => {
-        async function captureData () {
+        async function captureData() {
             const data = await getData();
             dispatch(updateAll(data))
         }
         captureData();
 
-      }, []);
+    }, []);
 
     useEffect(() => {
         setFilteredData(data);
@@ -67,12 +67,12 @@ export const Main: React.FC<Props>= (props: Props) => {
 
     useEffect(() => {
 
-            //run filter 1
-            const newData1 = filterByExactTopicMatch(data,filter);
+        //run filter 1
+        const newData1 = filterByExactTopicMatch(data, filter);
 
-            //run filter 2 for or logic
-            const newData2 = filterByGeneralMatch(newData1,orFitler);
-            setFilteredData(newData2)
+        //run filter 2 for or logic
+        const newData2 = filterByGeneralMatch(newData1, orFitler);
+        setFilteredData(newData2)
 
     }, [filter, orFitler]);
 
@@ -80,10 +80,10 @@ export const Main: React.FC<Props>= (props: Props) => {
         <>
             <Grid container spacing={2} className={classes.root} >
                 <Grid item spacing={0} xs={12}>
-                    <Filter onFilterChange={onFilterChange} onOrFilterChange={onOrFilterChange} matches={matches}/>
+                    <Filter onFilterChange={onFilterChange} onOrFilterChange={onOrFilterChange} matches={matches} />
                 </Grid>
                 <Grid item spacing={2} xs={12}>
-                    <LetterParagraph paragraphs={filteredData}/>
+                    <LetterParagraph paragraphs={filteredData} />
                 </Grid>
             </Grid>
         </>
