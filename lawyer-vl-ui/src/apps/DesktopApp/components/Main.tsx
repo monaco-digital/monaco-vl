0708b01e00e65083bdf5d3bcb8050345e0e42521
Abdur Rehman Khan <abrkhan10@gmail.com>
2020-11-16 10:11:51 +0000

    updating data

--- lawyer-vl-ui/src/apps/DesktopApp/components/Main.tsx
+++ lawyer-vl-ui/src/apps/DesktopApp/components/Main.tsx
@@ -113,24 +113,27 @@ export const Main: React.FC<Props> = (props: Props) => {
 								<div style={{ fontSize: '12' }}>
 									Instructions for use:
 									<br />
-									[1] Select the case features which apply to your case by
+									[1] Ensure you are using a desktop or laptop, as this doesn't
+									yet work on mobiles.
+									<br />
+									[2] Select the case features which apply to your case by
 									clicking the orange buttons below. This populates paragraphs
 									for you to choose from, on the left hand side of the screen.
 									<br />
-									[2] Drag and drop the relevant paragraphs from the left hand
+									[3] Drag and drop the relevant paragraphs from the left hand
 									side to the grey space on the right. Try not to use duplicate
 									paragraphs.
 									<br />
-									[3] When you’re happy with your chosen paragraphs, copy and
+									[4] When you’re happy with your chosen paragraphs, copy and
 									paste the contents (use the yellow ‘Copy Text’ button below
 									the template letter) onto a Word doc Google doc.
 									<br />
-									[4] Edit your draft letter, by filling in the gaps in square
+									[5] Edit your draft letter, by filling in the gaps in square
 									brackets. When filling in the gaps, do not exceed 3 short
 									bullet points for each piece of information requested. Try to
 									focus only on the very strongest points of your case.
 									<br />
-									[5] For the amount of money requested, use{' '}
+									[6] For the amount of money requested, use{' '}
 									<a
 										style={{ color: '#2962ff' }}
 										href="https://www.monacosolicitors.co.uk/free-settlement-agreement-calculator/"
@@ -139,7 +142,7 @@ export const Main: React.FC<Props> = (props: Props) => {
 										our settlement calculator
 									</a>
 									<br />
-									[6] To apply for our lawyer backed no win no fee service
+									[7] To apply for our lawyer backed no win no fee service
 									(terms and conditions{' '}
 									<a
 										style={{ color: '#2962ff' }}
