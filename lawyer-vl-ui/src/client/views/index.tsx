5f2c2cb37fc644d79c334e069f7acd8af3c01a8c
Venom (Mel Macaluso) <mel@capeeshe.com>
2020-12-04 12:32:25 -0300

    end of fix imports...

--- lawyer-vl-ui/src/client/views/index.tsx
+++ lawyer-vl-ui/src/client/views/index.tsx
@@ -1,23 +1,38 @@
-import React, { FC } from 'react'
+import React, { FC, useEffect } from 'react'
 import Footer from '../components/Footer'
 import LetterPreview from '../components/LetterPreview'
 import ParagraphsPreview from '../components/common/ParagraphsPreview'
 import Topics from '../components/common/Topics'
 import Header from '../components/Header'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import modes from '../state/modes'
 import ParagraphsEditMode from '../components/ParagraphsEditMode'
+import { setView } from '../../data/questionDataSlice'
+import { updateAllParagraphs } from '../../data/paragraphsDataSlice'
+import { getData } from '../../api/vlmasersheet'
 
 const Main: FC = () => {
 	const mode = useSelector<any, any>(state => state.questions.mode)
+	const dispatch = useDispatch()
+
+	useEffect(() => {
+		//TODO - fix this
+		dispatch(setView(undefined))
+		;(async () => {
+			const paragraphs = await getData()
+			dispatch(updateAllParagraphs(paragraphs))
+		})()
+	}, [])
 
 	return (
 		<main className="main">
 			<Header />
-			{mode === modes.TOPICS && <Topics />}
-			{mode === modes.PARAGRAPHS_PREVIEW && <ParagraphsPreview />}
-			{mode === modes.PARAGRAPHS_EDIT && <ParagraphsEditMode />}
-			{mode === modes.TOPICS && <LetterPreview />}
+			<div className="screen container">
+				{mode === modes.TOPICS && <Topics />}
+				{mode === modes.PARAGRAPHS_PREVIEW && <ParagraphsPreview />}
+				{mode === modes.PARAGRAPHS_EDIT && <ParagraphsEditMode />}
+				{mode === modes.LETTER_PREVIEW && <LetterPreview />}
+			</div>
 			<Footer />
 		</main>
 	)
