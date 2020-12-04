b984a7820d8cd632832b47732f9953ad262853ce
Venom (Mel Macaluso) <mel@capeeshe.com>
2020-12-04 12:40:28 -0300

    abdur to fix suggested paragraphs

--- lawyer-vl-ui/src/client/components/common/ParagraphsPreview/index.tsx
+++ lawyer-vl-ui/src/client/components/common/ParagraphsPreview/index.tsx
@@ -1,22 +1,10 @@
-import React, { FC, useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { FC } from 'react'
 import ParagraphsPreviewMobile from '../../mobile/ParagraphsPreview'
 import ParagraphsPreviewDesktop from '../../desktop/ParagraphsPreview'
 import useViewport from '../../../utils/useViewport'
-import { updateSuggestedParagraphs } from '../../../../data/paragraphsDataSlice'
-import AppState from '../../../../data/AppState'
-import { CaseTopic } from '../../../../data/types'
 
 const ParagraphsPreview: FC = () => {
 	const { isDesktop } = useViewport()
-	const selectedTopics = useSelector<AppState, CaseTopic[]>(
-		state => state.topics.selected
-	)
-	const dispatch = useDispatch()
-
-	useEffect(() => {
-		dispatch(updateSuggestedParagraphs(selectedTopics))
-	}, [selectedTopics])
 
 	return (
 		<>
