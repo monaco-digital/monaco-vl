2b0be20bc3761fcb8e4ab4ad340b427db589cf56
Abdur Rehman Khan <abrkhan10@gmail.com>
2020-12-06 16:00:11 +0000

    saving changes PROJ-1124

--- lawyer-vl-ui/src/client/components/Footer/index.tsx
+++ lawyer-vl-ui/src/client/components/Footer/index.tsx
@@ -7,40 +7,89 @@ import { useDispatch, useSelector } from 'react-redux'
 import { setView, setMode } from '../../../data/questionDataSlice'
 import AppState from '../../../data/AppState'
 import { CaseTopic } from '../../../data/types'
+import pages from '../../../types /navigation'
+import { setPage } from '../../../data/navigationDataSlice'
 
 const Footer: React.FC = () => {
-	const mode = useSelector<AppState, string>(state => state.questions.mode)
+	const page = useSelector<AppState, string>(state => state.navigation.page)
 	const selectedTopics = useSelector<AppState, CaseTopic[]>(
 		state => state.topics.selected
 	)
 	const dispatch = useDispatch()
 
-	const handleGoBack = () => {}
-	const handleMoreInfo = () => {}
-	const handleGoForward = () => {
-		dispatch(setView(selectedTopics))
-	}
+	// const handleGoForward = () => {
+	// 	dispatch(setView(selectedTopics))
+	// }
+
 	const enterLetterPreviewMode = () => {
-		dispatch(setMode(modes.LETTER_PREVIEW))
+		dispatch(setPage(pages.LETTER_PREVIEW))
 	}
+
+	const handleMoreInfo = () => {}
+
 	const enterParagraphPreviewMode = () => {
 		dispatch(setMode(modes.PARAGRAPHS_PREVIEW))
 	}
-	const enterParagraphEditMode = () => {
-		dispatch(setMode(modes.PARAGRAPHS_EDIT))
-	}
-	const downloadLetter = async () => {
-		// TODO find out how this is going to be implemented, if any.
-		// const docBlob = await createLetterDocx(selectedParagraphs)
-		// saveAs(docBlob, 'Your letter.docx')
-	}
+
+	// const enterParagraphEditMode = () => {
+	// 	dispatch(setMode(modes.PARAGRAPHS_EDIT))
+	// }
+
+	//const downloadLetter = async () => {
+	// TODO find out how this is going to be implemented, if any.
+	// const docBlob = await createLetterDocx(selectedParagraphs)
+	// saveAs(docBlob, 'Your letter.docx')
+	//}
 
 	return (
 		<footer className="footer">
-			<div className="container">
-				<div className="footer__actions">
-					{mode === modes.TOPICS && (
-						<>
+			<div className="footer__actions">
+				{page === pages.GET_STARTED && <></>}
+				{page === pages.TOPICS && (
+					<>
+						<button
+							className="footer__actions-info"
+							aria-label="More info"
+							type="button"
+							onClick={handleMoreInfo}
+						>
+							<img src={moreInfoIcon} />
+						</button>
+						{/*	<Button
+								type="green"
+								text="Next"
+								rounded
+								extraClasses="footer__actions-next"
+								fn={() => handleGoForward()}
+							/> */}
+					</>
+				)}
+				{page === pages.PARAGRAPHS_EDIT && (
+					<>
+						<Button
+							type="secondary"
+							text="Done"
+							rounded
+							fn={() => enterParagraphPreviewMode()}
+						/>
+						<button
+							className="footer__actions-drag"
+							aria-label="drag to reorder"
+						>
+							<img src={dragIcon} alt="" />
+							Drag to reorder
+						</button>
+					</>
+				)}
+				{page === pages.PARAGRAPHS_PREVIEW && (
+					<>
+						{/*	<Button
+								type="secondary"
+								text="Edit"
+								rounded
+								fn={() => enterParagraphEditMode()}
+							/>*/}
+						<div className="footer__switch__buttons">
 							<button
 								className="footer__actions-info"
 								aria-label="More info"
@@ -50,50 +99,32 @@ const Footer: React.FC = () => {
 								<img src={moreInfoIcon} />
 							</button>
 							<Button
-								type="green"
-								text="Next"
+								type="main"
+								text="Paragraphs"
 								rounded
-								extraClasses="footer__actions-next"
-								fn={() => handleGoForward()}
-							/>
-						</>
-					)}
-					{mode === modes.PARAGRAPHS_EDIT && (
-						<>
-							<Button
-								type="secondary"
-								text="Done"
-								rounded
-								fn={() => enterParagraphPreviewMode()}
-							/>
-							<button
-								className="footer__actions-drag"
-								aria-label="drag to reorder"
-							>
-								<img src={dragIcon} alt="" />
-								Drag to reorder
-							</button>
-						</>
-					)}
-					{mode === modes.PARAGRAPHS_PREVIEW && (
-						<>
-							<Button
-								type="secondary"
-								text="Edit"
-								rounded
-								fn={() => enterParagraphEditMode()}
+								fn={() => enterLetterPreviewMode()}
 							/>
 							<Button
-								type="green"
+								type="main"
+								text="Summaries"
+								rounded
+								fn={() => enterLetterPreviewMode()}
+							/>
+						</div>
+
+						<div>
+							<Button
+								type="main"
 								text="Preview Letter"
 								rounded
 								fn={() => enterLetterPreviewMode()}
 							/>
-						</>
-					)}
-					{mode === modes.LETTER_PREVIEW && (
-						<>
-							<Button
+						</div>
+					</>
+				)}
+				{page === pages.LETTER_PREVIEW && (
+					<>
+						{/*							<Button
 								type="secondary"
 								text="Edit"
 								rounded
@@ -104,13 +135,33 @@ const Footer: React.FC = () => {
 								text="Create Document"
 								rounded
 								fn={() => downloadLetter()}
+							/>*/}
+						<button
+							className="footer__actions-info"
+							aria-label="More info"
+							type="button"
+							onClick={handleMoreInfo}
+						>
+							<img src={moreInfoIcon} />
+						</button>
+						<div>
+							<Button
+								type="secondary"
+								text="Copy letter text"
+								rounded
+								fn={() => {}}
 							/>
-						</>
-					)}
-				</div>
+							<Button
+								type="main"
+								text="Create Google Doc"
+								rounded
+								fn={() => {}}
+							/>
+						</div>
+					</>
+				)}
 			</div>
 		</footer>
 	)
 }
-
 export default Footer
