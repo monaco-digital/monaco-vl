a74c7428d71a0039b04f07f2ab5befe499ce3ffa
Abdur Rehman Khan <abrkhan10@gmail.com>
2021-03-12 09:23:39 +0000

    PROJ-1402 saving changes

--- src/client/views/GetStarted.tsx
+++ src/client/views/GetStarted.tsx
@@ -11,11 +11,19 @@ export const GetStarted: React.FC = () => {
 	}
 
 	return (
-		<div className="get-started">
-			<h1>Empowering employees</h1>
-			<h3>Get free advice and build legal letters when badly treated at work</h3>
-			<img style={{ height: '300px' }} src={mainimage} />
-			<Button type="start" text="Get started" rounded fn={goToTopics} />
+		<div className="get-started space-y-3 w-full">
+			<div>
+				<h1>Empowering employees</h1>
+			</div>
+			<div>
+				<h3>Get free advice and build legal letters when badly treated at work</h3>
+			</div>
+			<div className="get-started__content">
+				<img style={{ height: '300px' }} src={mainimage} />
+			</div>
+			<div className="flex justify-center">
+				<Button type="start" text="Get started" rounded fn={goToTopics} />
+			</div>
 		</div>
 	)
 }
