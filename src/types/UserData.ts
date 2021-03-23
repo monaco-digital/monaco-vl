aea5ab4c3ed2be77364f5e277602b6fafa62402b
Abdur Rehman Khan <abrkhan10@gmail.com>
2021-03-23 11:34:08 +0000

    PROJ-1401 linting fixes

--- src/types/UserData.ts
+++ src/types/UserData.ts
@@ -1,15 +1,15 @@
 export interface UserData {
-	adviceText?: string
-	letterText?: string
-	topicsList?: string
-	name?: string
-	recipient?: string
-	contactMe?: boolean
-	description?: string
-	phone?: string
-	salary?: string
-	settlementAgreement?: string
-	stillEmployed?: string
-	yearsEmployed?: string
-	templateId?: string
+	adviceText?: string;
+	letterText?: string;
+	topicsList?: string;
+	name?: string;
+	recipient?: string;
+	contactMe?: boolean;
+	description?: string;
+	phone?: string;
+	salary?: string;
+	settlementAgreement?: string;
+	stillEmployed?: string;
+	yearsEmployed?: string;
+	templateId?: string;
 }
