78dbb4636aaabc10c2fea27d837ba3501255d86f
Slothzieee <silviosperanza97@gmail.com>
2021-03-25 16:07:24 +0100

    using NavLink for VL icon redirect

--- src/client/components/common/Header/index.tsx
+++ src/client/components/common/Header/index.tsx
@@ -2,13 +2,10 @@ import React, { FC, useState } from 'react';
 import { NavLink, useLocation } from 'react-router-dom';
 import { Drawer, List, ListItem, ListItemText } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
-import config from '../../../../config/index';
 
 import logo2 from '../../../assets/img/vl-powered-by-logo-01.svg';
 import logo1 from '../../../assets/img/ms-logo-blue-black.svg';
 import { removeLastAnsweredQuestion } from '../../../../data/sessionDataSlice';
-
-const { VL_URL } = config;
 
 const Header: FC = () => {
 	const { pathname } = useLocation();
@@ -25,9 +22,9 @@ const Header: FC = () => {
 			<a href="https://www.monacosolicitors.co.uk/?from=vl-ui&source=mobile" target="_blank" rel="noreferrer">
 				<img className="header__logo-1" alt="Virtual lawyer" src={logo1} />
 			</a>
-			<a href={VL_URL} rel="noreferrer">
+			<NavLink to="/">
 				<img className="header__logo-2" alt="Virtual lawyer" src={logo2} />
-			</a>
+			</NavLink>
 			<div className="header__breadcrumb">
 				<NavLink
 					to="/questions"
