5d599d5a201bb48bc9d22ae7c531430b2df0984c
Paul Grimmer <paul.grimmer@monacosolicitors.co.uk>
2021-04-09 16:31:39 +0100

    PROJ-1543 no-copy all preview screens

--- src/client/components/common/DocumentPreview/index.tsx
+++ src/client/components/common/DocumentPreview/index.tsx
@@ -60,8 +60,6 @@ const DocumentPreview: FC = () => {
 	const selectedTemplate = useSelector<AppState, Template>(state => state.session.selectedTemplate);
 	const isDsFlow = useSelector<AppState, boolean>(state => state.features.dsFlow);
 
-	const isBlur = isMonetizationEnabled && selectedTopics.some(({ id }) => id === '_LET');
-
 	const updatedTemplate = getTemplate(selectedTopics);
 
 	const sessionDocument = useSelector<AppState, SessionDocument>(state => state.session.sessionDocument);
@@ -97,7 +95,7 @@ const DocumentPreview: FC = () => {
 	return (
 		<>
 			<div className="letter-preview">
-				<VLcard heading="Draft letter" theme="light" counter={selectedParagraphs.length} blur={isBlur}>
+				<VLcard heading="Draft letter" theme="light" counter={selectedParagraphs.length}>
 					<div className="letter-preview__body">
 						<SessionDocComponents sessionDocumentComponents={sessionDocument?.sessionDocumentComponents} />
 					</div>
