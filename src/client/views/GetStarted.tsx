02189ab51409ba5537996d6f27006297e3651ddc
dvellazarbms <82370879+dvellazarbms@users.noreply.github.com>
2021-04-13 16:19:23 +0100

    Update homepage UI

--- src/client/views/GetStarted.tsx
+++ src/client/views/GetStarted.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import Grid from '@material-ui/core/Grid';
+import { Button as MUIButton } from '@material-ui/core';
 import Button from '../components/Button';
 import mainimage from '../assets/img/vl-labels-illustration.svg';
+import lady_reading_letter_image from '../assets/img/Lady reading letter@2x.png';
 
 const GetStarted: React.FC = () => {
 	const history = useHistory();
@@ -28,6 +31,33 @@ const GetStarted: React.FC = () => {
 				<div className="get-started__button-mobile flex justify-center">
 					<Button type="start" text="Get started" rounded fn={goToTopics} />
 				</div>
+			</div>
+			<div>
+				<h2>How can we help you?</h2>
+			</div>
+			<div>
+				<Grid container direction="row" justify="center" alignItems="center" spacing={10}>
+					<Grid item>
+						<div>
+							<h4>Get a free customised advice letter</h4>
+							<div className="get-started__find-out-more-bullets">
+								<ul className="list-disc">
+									<li>No need to sign up</li>
+									<li>Get a free customised advice letter</li>
+									<li>Customised advice letter in less than 5 minutes</li>
+								</ul>
+							</div>
+							<div>
+								<MUIButton variant="outlined">Find out more</MUIButton>
+							</div>
+						</div>
+					</Grid>
+					<Grid item>
+						<div className="get-started__grid-image">
+							<img src={lady_reading_letter_image} alt="" />
+						</div>
+					</Grid>
+				</Grid>
 			</div>
 		</div>
 	);
