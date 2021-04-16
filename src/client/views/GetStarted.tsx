80892423d56d11fc3d062f5b1380c602d34a634f
dvellazarbms <82370879+dvellazarbms@users.noreply.github.com>
2021-04-16 13:38:31 +0100

    Updates homepage UI

--- src/client/views/GetStarted.tsx
+++ src/client/views/GetStarted.tsx
@@ -1,8 +1,7 @@
 import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
-import { Button as MUIButton } from '@material-ui/core';
-import Button from '../components/Button';
+import { Button, Typography } from '@material-ui/core';
 import mainimage from '../assets/img/vl-labels-illustration.svg';
 import lady_reading_letter_image from '../assets/img/Lady reading letter@2x.png';
 import lawyer_signing_doc_image from '../assets/img/Lawyer signing doc@2x.png';
@@ -19,15 +18,11 @@ const GetStarted: React.FC = () => {
 		history.push('/questions');
 	};
 
-	const goToHelp = () => {
-		history.push('/help');
-	};
-
-	const FindOutMoreButton = () => {
+	const GetStartedButton = () => {
 		return (
-			<MUIButton variant="outlined" className="get-started__button-find-out-more" onClick={goToHelp}>
-				Find out more
-			</MUIButton>
+			<Button variant="contained" className="get-started__get-started-button" color="primary" onClick={goToTopics}>
+				Get Started
+			</Button>
 		);
 	};
 
@@ -46,41 +41,44 @@ const GetStarted: React.FC = () => {
 		<div className="get-started space-y-3 w-full">
 			<div className="md:flex md:flex-shrink">
 				<div className="md:flex md:flex-row space-x-20">
-					<div>
-						<h1>Empowering employees</h1>
-						<h3>Treated unfairly at work? Get free advice & build a bespoke legal letter in less than 10 minutes.</h3>
-						<div className="get-started__button-desktop">
-							<Button type="start" text="Get started" rounded fn={goToTopics} />
+					<div className="get-started__heading-box">
+						<div>
+							<Typography variant="h1">
+								Automated <br /> Legal
+								<br />
+								EXperience
+							</Typography>
+						</div>
+						<div className="get-started__caption">
+							<Typography variant="h3">How to win at work in 4 easy steps</Typography>
+						</div>
+						<div className="get-started__first-button">
+							<GetStartedButton />
 						</div>
 					</div>
 					<div className="get-started__image">
 						<img className="object-cover" src={mainimage} alt="" />
 					</div>
 				</div>
-				<div className="get-started__button-mobile flex justify-center">
-					<Button type="start" text="Get started" rounded fn={goToTopics} />
-				</div>
 			</div>
 
-			<div id="text-banner-widget" />
-
-			<div>
-				<h2>How can we help you?</h2>
-			</div>
+			<div id="text-banner-widget" className="get-started__reviews-widget" />
 
 			<div className="get-started__grid-row">
 				<Grid container justify="center" alignItems="flex-start" spacing={10} className="get-started__grid-container">
 					<Grid item className="get-start__grid-item">
 						<div className="get-started__find-out-more">
-							<h4>Get a free customised advice letter</h4>
-							<div className="get-started__find-out-more-bullets">
+							<Typography variant="h4">Get a free customised advice letter</Typography>
+							<div className="get-started__bullets">
 								<ul className="list-disc">
-									<li>No need to sign up</li>
-									<li>Get a free customised advice letter</li>
-									<li>Customised advice letter in less than 5 minutes</li>
+									<Typography variant="body1">
+										<li>No need to sign up</li>
+										<li>Get a free customised advice letter</li>
+										<li>Customised advice letter in less than 5 minutes</li>
+									</Typography>
 								</ul>
 							</div>
-							<FindOutMoreButton />
+							<GetStartedButton />
 						</div>
 					</Grid>
 					<Grid item className="get-start__grid-item">
@@ -105,15 +103,17 @@ const GetStarted: React.FC = () => {
 					</Grid>
 					<Grid item className="get-start__grid-item">
 						<div className="get-started__find-out-more" style={{ marginLeft: '1.5rem' }}>
-							<h4>Respond to a legal letter</h4>
-							<div className="get-started__find-out-more-bullets">
+							<Typography variant="h4">Respond to a legal letter</Typography>
+							<div className="get-started__bullets">
 								<ul className="list-disc">
-									<li>No need to sign up</li>
-									<li>Formal grievance process</li>
-									<li>Legally accurate letter in less than 5 minutes</li>
+									<Typography variant="body1">
+										<li>No need to sign up</li>
+										<li>Formal grievance process</li>
+										<li>Legally accurate letter in less than 5 minutes</li>
+									</Typography>
 								</ul>
 							</div>
-							<FindOutMoreButton />
+							<GetStartedButton />
 						</div>
 					</Grid>
 				</Grid>
@@ -122,15 +122,17 @@ const GetStarted: React.FC = () => {
 				<Grid container justify="center" alignItems="flex-start" spacing={10} className="get-started__grid-container">
 					<Grid item className="get-start__grid-item">
 						<div className="get-started__find-out-more">
-							<h4>Build a formal grievance letter</h4>
-							<div className="get-started__find-out-more-bullets">
+							<Typography variant="h4">Build a formal grievance letter</Typography>
+							<div className="get-started__bullets">
 								<ul className="list-disc">
-									<li>No need to sign up</li>
-									<li>Formal grievance process</li>
-									<li>Legally accurate letter in less than 5 minutes</li>
+									<Typography variant="body1">
+										<li>No need to sign up</li>
+										<li>Formal grievance process</li>
+										<li>Legally accurate letter in less than 5 minutes</li>
+									</Typography>
 								</ul>
 							</div>
-							<FindOutMoreButton />
+							<GetStartedButton />
 						</div>
 					</Grid>
 					<Grid item className="get-start__grid-item">
@@ -155,18 +157,20 @@ const GetStarted: React.FC = () => {
 					</Grid>
 					<Grid item className="get-start__grid-item">
 						<div className="get-started__find-out-more" style={{ marginLeft: '1.5rem' }}>
-							<h4>
+							<Typography variant="h4">
 								Build a &apos;without prejudice&apos; letter <br />
 								negotiating an exit package
-							</h4>
-							<div className="get-started__find-out-more-bullets">
+							</Typography>
+							<div className="get-started__bullets">
 								<ul className="list-disc">
-									<li>Off the record</li>
-									<li>Without prejudice procedure</li>
-									<li>Legally accurate letter in less than 5 minutes</li>
+									<Typography variant="body1">
+										<li>Off the record</li>
+										<li>Without prejudice procedure</li>
+										<li>Legally accurate letter in less than 5 minutes</li>
+									</Typography>
 								</ul>
 							</div>
-							<FindOutMoreButton />
+							<GetStartedButton />
 						</div>
 					</Grid>
 				</Grid>
