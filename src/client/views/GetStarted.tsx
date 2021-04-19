aa7b4c3e043e95ae51b6e60ccdae5e4a09373616
dvellazarbms <82370879+dvellazarbms@users.noreply.github.com>
2021-04-19 15:09:11 +0100

    Updates homepage UI

--- src/client/views/GetStarted.tsx
+++ src/client/views/GetStarted.tsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import { Button, Typography } from '@material-ui/core';
+import { mdiNumeric1Circle, mdiNumeric2Circle, mdiNumeric3Circle, mdiNumeric4Circle } from '@mdi/js';
+import Icon from '@mdi/react';
 import mainimage from '../assets/img/vl-labels-illustration.svg';
 import lady_reading_letter_image from '../assets/img/Lady reading letter@2x.png';
 import lawyer_signing_doc_image from '../assets/img/Lawyer signing doc@2x.png';
@@ -13,6 +15,8 @@ declare var richSnippetReviewsWidgets;
 
 const GetStarted: React.FC = () => {
 	const history = useHistory();
+
+	const iconColour = '#60ABFF';
 
 	const goToTopics = () => {
 		history.push('/questions');
@@ -44,13 +48,11 @@ const GetStarted: React.FC = () => {
 					<div className="get-started__heading-box">
 						<div>
 							<Typography variant="h1">
-								Automated <br /> Legal
-								<br />
-								EXperience
+								Automated <br /> Legal <br /> EXperience
 							</Typography>
 						</div>
 						<div className="get-started__caption">
-							<Typography variant="h3">How to win at work in 4 easy steps</Typography>
+							<Typography variant="h5">How to win at work in 4 easy steps</Typography>
 						</div>
 						<div className="get-started__first-button">
 							<GetStartedButton />
@@ -67,8 +69,17 @@ const GetStarted: React.FC = () => {
 			<div className="get-started__grid-row">
 				<Grid container justify="center" alignItems="flex-start" spacing={10} className="get-started__grid-container">
 					<Grid item className="get-start__grid-item">
-						<div className="get-started__find-out-more">
-							<Typography variant="h4">Get a free customised advice letter</Typography>
+						<div className="get-started__details">
+							<div style={{ display: 'flex', alignItems: 'center' }}>
+								<Icon
+									path={mdiNumeric1Circle}
+									title="1 icon"
+									size={1.2}
+									color={iconColour}
+									style={{ marginRight: '0.3rem' }}
+								/>
+								<Typography variant="h4">Get a free customised advice letter</Typography>
+							</div>
 							<div className="get-started__bullets">
 								<ul className="list-disc">
 									<Typography variant="body1">
@@ -102,8 +113,17 @@ const GetStarted: React.FC = () => {
 						</div>
 					</Grid>
 					<Grid item className="get-start__grid-item">
-						<div className="get-started__find-out-more" style={{ marginLeft: '1.5rem' }}>
-							<Typography variant="h4">Respond to a legal letter</Typography>
+						<div className="get-started__details-alternate">
+							<div style={{ display: 'flex', alignItems: 'center' }}>
+								<Icon
+									path={mdiNumeric2Circle}
+									title="2 icon"
+									size={1.2}
+									color={iconColour}
+									style={{ marginRight: '0.3rem' }}
+								/>
+								<Typography variant="h4">Respond to a legal letter</Typography>
+							</div>
 							<div className="get-started__bullets">
 								<ul className="list-disc">
 									<Typography variant="body1">
@@ -121,8 +141,17 @@ const GetStarted: React.FC = () => {
 			<div className="get-started__grid-row">
 				<Grid container justify="center" alignItems="flex-start" spacing={10} className="get-started__grid-container">
 					<Grid item className="get-start__grid-item">
-						<div className="get-started__find-out-more">
-							<Typography variant="h4">Build a formal grievance letter</Typography>
+						<div className="get-started__details">
+							<div style={{ display: 'flex', alignItems: 'center' }}>
+								<Icon
+									path={mdiNumeric3Circle}
+									title="3 icon"
+									size={1.2}
+									color={iconColour}
+									style={{ marginRight: '0.3rem' }}
+								/>
+								<Typography variant="h4">Build a formal grievance letter</Typography>
+							</div>
 							<div className="get-started__bullets">
 								<ul className="list-disc">
 									<Typography variant="body1">
@@ -156,11 +185,17 @@ const GetStarted: React.FC = () => {
 						</div>
 					</Grid>
 					<Grid item className="get-start__grid-item">
-						<div className="get-started__find-out-more" style={{ marginLeft: '1.5rem' }}>
-							<Typography variant="h4">
-								Build a &apos;without prejudice&apos; letter <br />
-								negotiating an exit package
-							</Typography>
+						<div className="get-started__details-alternate">
+							<div style={{ display: 'flex', alignItems: 'center' }}>
+								<Icon
+									path={mdiNumeric4Circle}
+									title="4 icon"
+									size={1.2}
+									color={iconColour}
+									style={{ marginRight: '0.3rem' }}
+								/>
+								<Typography variant="h4">Reach a settlement</Typography>
+							</div>
 							<div className="get-started__bullets">
 								<ul className="list-disc">
 									<Typography variant="body1">
