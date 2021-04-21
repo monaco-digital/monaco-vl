02e026fe4166b8876cbb5ec8a80664f34e6a3529
dvellazarbms <82370879+dvellazarbms@users.noreply.github.com>
2021-04-21 17:22:04 +0100

    Adds Step 3 Intro page

--- src/client/components/common/Step3Intro/index.tsx
+++ src/client/components/common/Step3Intro/index.tsx
@@ -1,41 +1,62 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { mdiNumeric3Circle } from '@mdi/js';
 import Icon from '@mdi/react';
-import { Button, Fab, Typography } from '@material-ui/core';
+import { Fab, Typography } from '@material-ui/core';
 
 const Step3Intro: React.FC = () => {
 	const iconColour = '#60ABFF';
 	const iconSize = 1.2;
 
+	const [selectedOption, setSelectedOption] = useState('');
+
+	const optionsToShow = [
+		{ text: 'My employer has replied to my letter', optionId: '0' },
+		{ text: 'My employer is ignoring me', optionId: '1' },
+		{ text: 'My 3 month time limit is running out', optionId: '2' },
+	];
+
+	const options = optionsToShow.map(option => {
+		const { text } = option;
+		const { optionId } = option;
+
+		return (
+			<div key={optionId} className="topic inline-flex">
+				<input
+					type="checkbox"
+					id={optionId}
+					name={text}
+					value={text}
+					onChange={() => setSelectedOption(optionId)}
+					checked={selectedOption === optionId}
+				/>
+				<label htmlFor={optionId}>
+					<div className="inline-flex items-center content-center">{text}</div>
+				</label>
+			</div>
+		);
+	});
+
 	return (
-		<div className="step-2-intro">
-			<div className="step-2-intro__icon-and-header">
+		<div className="step-3-intro">
+			<div className="step-3-intro__icon-and-header">
 				<Icon
 					path={mdiNumeric3Circle}
 					title="3 icon"
 					size={iconSize}
 					color={iconColour}
-					className="step-2-intro__number-icon"
+					className="step-3-intro__number-icon"
 				/>
 				<Typography variant="h4">Progress legal case</Typography>
 			</div>
-			<div className="step-2-intro__bullets">
-				<Typography variant="body1">You now have 3 options depending on how your employer reacts</Typography>
+			<div className="step-3-intro__text">
+				<Typography variant="body1">You now have 3 options depending on how your employer reacts:</Typography>
 			</div>
-			<div>
-				<Button variant="contained">
-					View template letter for responding to your employer (if your employer has replied to your letter)
-				</Button>
-				<Button variant="contained">View template formal grievance letter (if your employer is ignoring you)</Button>
-				<Button variant="contained">
-					View template employment tribunal document (if your three month time limit is running out)
-				</Button>
-			</div>
-			<div className="step-2-intro__buttons">
-				<Fab variant="extended" color="inherit" className="step-2-intro__button">
+			<div className="step-3-intro__options">{options}</div>
+			<div className="step-3-intro__buttons">
+				<Fab variant="extended" color="inherit" className="step-3-intro__button">
 					Back
 				</Fab>
-				<Fab variant="extended" color="secondary">
+				<Fab variant="extended" color="secondary" disabled={selectedOption === ''}>
 					Next
 				</Fab>
 			</div>
