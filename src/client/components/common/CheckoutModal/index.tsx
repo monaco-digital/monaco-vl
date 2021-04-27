825b80b5e05b28643ab94a78c78c409c4885ec3c
dvellazarbms <82370879+dvellazarbms@users.noreply.github.com>
2021-04-27 17:19:05 +0100

    Removes unused imports

--- src/client/components/common/CheckoutModal/index.tsx
+++ src/client/components/common/CheckoutModal/index.tsx
@@ -4,10 +4,7 @@ import IconButton from '@material-ui/core/IconButton';
 import CancelOutlinedIcon from '@material-ui/icons/CancelOutlined';
 import { Dialog, useMediaQuery, useTheme } from '@material-ui/core';
 import EmailModal from '../EmailModal';
-import Upsell from '../Payment/Upsell';
-import PaymentComplete from '../Payment/PaymentComplete';
 import EmailComplete from '../EmailModal/EmailComplete';
-import { PaymentForm } from '../Payment/PaymentForm';
 import { CDF1 } from '../UserData/CDF1';
 import CDFComplete from '../UserData/CDFComplete';
 
