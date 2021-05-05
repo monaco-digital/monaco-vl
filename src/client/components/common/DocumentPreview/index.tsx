43aa2b1a8b09717f9ab662379743d6ef92061386
Paul Grimmer <paul.grimmer@monacosolicitors.co.uk>
2021-05-05 16:41:17 +0100

    PROJ-1549 - Make next button work for tribunal preview

--- src/client/components/common/DocumentPreview/index.tsx
+++ src/client/components/common/DocumentPreview/index.tsx
@@ -129,6 +129,7 @@ const DocumentPreview: FC = () => {
 			case '_WP':
 				history.push('/progress-legal-case'); // Go to step 3
 				break;
+			case '_ET':
 			case '_GR':
 			case '_RES_CD':
 			case '_RES_CO':
