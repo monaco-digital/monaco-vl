cc168dadeb10554ebb92754f900f115a74cfb8f7
dvellazarbms <82370879+dvellazarbms@users.noreply.github.com>
2021-05-06 12:03:40 +0100

    Updates homepage

---
+++ src/client/components/common/GetStartedButton/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Button } from '@material-ui/core';
+import { useHistory } from 'react-router-dom';
+
+const GetStartedButton: React.FC = () => {
+	const history = useHistory();
+
+	const handleClick = () => history.push('/questions');
+
+	return (
+		<Button
+			variant="contained"
+			className="get-started__get-started-button"
+			color="primary"
+			size="large"
+			onClick={handleClick}
+		>
+			Get Started
+		</Button>
+	);
+};
+
+export default GetStartedButton;
