83a2c60b3d9c8d7b918c218890f97d8012345a34
Slothzieee <silviosperanza97@gmail.com>
2021-05-13 12:09:41 +0200

    fixed commented sections

--- src/client/components/common/PreviewLetterExplanation/index.tsx
+++ src/client/components/common/PreviewLetterExplanation/index.tsx
@@ -10,55 +10,44 @@ const PreviewLetterExplanation: React.FC<Props> = ({ letter }: Props) => {
 		letterType = '_RES';
 	}
 
-	const lettersExplanationText = [
-		{
-			_GR: (
-				<i>
-					Well done! You have generated a grievance letter to send to your employer. Email this to yourself with the
-					button below to fill in later. Then proceed to the next stage.
-				</i>
-			),
-		},
-		{
-			_ET: (
-				<i>
-					Well done! You have generated a Grounds of Claim document to send to the tribunal. Email this to yourself with
-					the button below to fill in later. Then proceed to the next stage.
-				</i>
-			),
-		},
-		{
-			_RES: (
-				<i>
-					Well done! You have generated a further letter to send to your employer. Email this to yourself with the
-					button below to fill in later. Then proceed to the next stage.
-				</i>
-			),
-		},
-		{
-			_WP: (
-				<i>
-					Well done! You have completed stage 2 of 4 and generated a{' '}
-					<a
-						href="https://www.monacosolicitors.co.uk/negotiations/how-to-use-without-prejudice/"
-						target="_blank"
-						rel="nofollow noreferrer"
-					>
-						<b>without prejudice</b>
-					</a>{' '}
-					legal letter, below. Email this to yourself with the button below to fill in later. Then proceed to the next
-					stage.
-				</i>
-			),
-		},
-	];
-
-	const explanationText = lettersExplanationText.map(e => e[letterType]);
-	console.log(letterType);
+	const lettersExplanationText = {
+		_GR: (
+			<i>
+				Well done! You have generated a grievance letter to send to your employer. Email this to yourself with the
+				button below to fill in later. Then proceed to the next stage.
+			</i>
+		),
+		_ET: (
+			<i>
+				Well done! You have generated a Grounds of Claim document to send to the tribunal. Email this to yourself with
+				the button below to fill in later. Then proceed to the next stage.
+			</i>
+		),
+		_RES: (
+			<i>
+				Well done! You have generated a further letter to send to your employer. Email this to yourself with the button
+				below to fill in later. Then proceed to the next stage.
+			</i>
+		),
+		_WP: (
+			<i>
+				Well done! You have completed stage 2 of 4 and generated a{' '}
+				<a
+					href="https://www.monacosolicitors.co.uk/negotiations/how-to-use-without-prejudice/"
+					target="_blank"
+					rel="nofollow noreferrer"
+				>
+					<b>without prejudice</b>
+				</a>{' '}
+				legal letter, below. Email this to yourself with the button below to fill in later. Then proceed to the next
+				stage.
+			</i>
+		),
+	};
 
 	return (
 		<div className="letter-explanation">
-			<p>{explanationText}</p>
+			<p>{lettersExplanationText[letterType]}</p>
 		</div>
 	);
 };
