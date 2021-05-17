551f2e4617660b59b6ea5a8c9d5644568b7a6663
dvellazarbms <82370879+dvellazarbms@users.noreply.github.com>
2021-05-17 16:54:18 +0100

    Fixes scrolling issue

--- src/client/components/common/EndToEndStepper/index.tsx
+++ src/client/components/common/EndToEndStepper/index.tsx
@@ -1,27 +1,33 @@
 import React from 'react';
-import { Stepper, Step, StepLabel } from '@material-ui/core';
+import { Stepper, Step, StepLabel, useMediaQuery } from '@material-ui/core';
+import { useTheme } from '@material-ui/core/styles';
 
 interface Props {
 	step: number;
 }
 
 const EndToEndStepper: React.FC<Props> = ({ step }: Props) => {
+	const theme = useTheme();
+	const isSmall = useMediaQuery(theme.breakpoints.down('sm'));
+
 	return (
 		<div className="end-to-end-stepper">
-			<Stepper activeStep={step} data-testid="stepper-component">
-				<Step>
-					<StepLabel>Advice</StepLabel>
-				</Step>
-				<Step>
-					<StepLabel>Legal</StepLabel>
-				</Step>
-				<Step>
-					<StepLabel>Document</StepLabel>
-				</Step>
-				<Step>
-					<StepLabel>Settlement</StepLabel>
-				</Step>
-			</Stepper>
+			{!isSmall && (
+				<Stepper activeStep={step} data-testid="stepper-component">
+					<Step>
+						<StepLabel>Advice</StepLabel>
+					</Step>
+					<Step>
+						<StepLabel>Legal</StepLabel>
+					</Step>
+					<Step>
+						<StepLabel>Document</StepLabel>
+					</Step>
+					<Step>
+						<StepLabel>Settlement</StepLabel>
+					</Step>
+				</Stepper>
+			)}
 		</div>
 	);
 };
