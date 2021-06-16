2dde1a5a794667caa4f569b5020885048d79ca79
dvellazarbms <82370879+dvellazarbms@users.noreply.github.com>
2021-06-16 15:48:10 +0100

    refactor: removes unnecessary code

--- src/client/components/common/VLcard/index.tsx
+++ src/client/components/common/VLcard/index.tsx
@@ -1,17 +1,15 @@
 import React, { FC, ReactNode } from 'react';
 import classNames from 'classnames';
 import { useSelector } from 'react-redux';
-import expandTextIcon from '../../../assets/img/expand-text-icon.svg';
 import AppState from '../../../../data/AppState';
 
 type VLCardProps = {
 	children: ReactNode;
 	theme?: 'light' | 'dark';
 	heading: string;
-	counter?: number;
 };
 
-const VLcard: FC<VLCardProps> = ({ children, heading, counter, theme }: VLCardProps) => {
+const VLcard: FC<VLCardProps> = ({ children, heading, theme }: VLCardProps) => {
 	const enableSelect = useSelector<AppState, boolean>(state => state.features.enableSelect);
 
 	const VLCardClasses = classNames('vl-card', {
@@ -22,12 +20,6 @@ const VLcard: FC<VLCardProps> = ({ children, heading, counter, theme }: VLCardPr
 		<div className={VLCardClasses}>
 			<div className="vl-card__header">
 				<span className="vl-card__header__title">{heading}</span>
-				{counter !== undefined && (
-					<span className="vl-card__header__counter">
-						<img src={expandTextIcon} alt="Expand Text" />
-						<span className="vl-card__header__counter-number">{counter}</span>
-					</span>
-				)}
 			</div>
 			<div className={enableSelect ? 'vl-card__enable-select' : ''}>{children}</div>
 		</div>
@@ -36,7 +28,6 @@ const VLcard: FC<VLCardProps> = ({ children, heading, counter, theme }: VLCardPr
 
 VLcard.defaultProps = {
 	theme: 'light',
-	counter: undefined,
 };
 
 export default VLcard;
