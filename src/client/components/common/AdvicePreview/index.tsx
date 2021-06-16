b2d8082e9dff9d33f12903928fc6991653def2a3
dvellazarbms <82370879+dvellazarbms@users.noreply.github.com>
2021-06-16 13:32:55 +0100

    feat: removes counter for number of paragraphs in advice/preview letters

--- src/client/components/common/AdvicePreview/index.tsx
+++ src/client/components/common/AdvicePreview/index.tsx
@@ -58,7 +58,7 @@ const AdvicePreview: FC = () => {
 				<EndToEndStepper step={0} />
 				<div className="letter-preview">
 					<PreviewAdviceExplanation />
-					<VLcard heading="Your advice note" theme="light" counter={adviceParagraphs.length}>
+					<VLcard heading="Your advice note" theme="light">
 						<div className="letter-preview__body">
 							{adviceParagraphs.map(paragraph => (
 								<AdviceDocParagraph key={paragraph.id} paragraph={paragraph} />
